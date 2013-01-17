d32670506bdd5871634748d94c409b62ee942c40
Opscode <opscode@ip-10-90-150-120.ec2.internal>
2013-01-17 09:22:23 +0000

    workshop end of day

--- roles/base.rb
+++ roles/base.rb
@@ -2,11 +2,31 @@ name "base"
 description "Base role applied to all nodes."
 run_list(
   "recipe[apt]",
-  "recipe[nagios::client]"
+  "recipe[nagios::client]",
+  "recipe[chef-client::delete_validation]",
+  "recipe[motd-tail]",
+  "recipe[ntp]",
+  "recipe[postfix::client]",
+  "recipe[zsh]",
+  "recipe[users::sysadmins]", #requires zsh
+  "recipe[sudo]"
 )
 
 default_attributes(
   "nagios" => {
     "server_role" => "monitoring"
-  }
+  },
+  "authorization" => {
+    "sudo" => {
+      "users" => ["opscode"],
+      "groups" => ["admin","sudo"],
+      "passwordless" => true
+                     }
+                   },
+
+  "postfix" => {
+    "relayhost_role" => "monitoring"
+                   }
+
+
 )
--- roles/monitoring.rb
+++ roles/monitoring.rb
@@ -1,11 +1,19 @@
 name "monitoring"
 description "Monitoring server"
 run_list(
-  "recipe[nagios::server]"
+  "recipe[nagios::server]",
+  "recipe[postfix::server]"
 )
 
 default_attributes(
   "nagios" => {
     "server_auth_method" => "htauth"
-  }
+  },
+  "ntp" => {
+    "is_server" => true,
+                   },
+                   "postfix" => {
+                     "mydomain" => "example.com",
+                     "mail_relay_networks" => "127.0.0.0/8 10.12.13.0/24"
+                   }
 )
